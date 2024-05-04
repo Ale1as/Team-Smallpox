faad441b216004ca5081fe8ed08b9df7467314d9
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-05-04 23:15:51 +0530

    player movement and animation

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-var speed:int = 100
-var max_speed = 10
+var speed:int = 200
+
+@onready var animated_sprite = $AnimatedSprite2D
 func handleInput():
 	var moveDir = Input.get_vector("left","right","up","down")
 	velocity = moveDir * speed
@@ -9,5 +10,22 @@ func handleInput():
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
-	look_at(get_global_mouse_position())
-	rotation_degrees +=90
+	if(Input.is_action_pressed("down")):
+		animated_sprite.play("front_walk")
+	elif(Input.is_action_just_released("down")):
+		animated_sprite.play("front")
+		
+	if(Input.is_action_pressed("up")):
+		animated_sprite.play("back_walk")
+	elif(Input.is_action_just_released("up")):
+		animated_sprite.play("back")
+		
+	if(Input.is_action_pressed("left")):
+		animated_sprite.play("left_walk")
+	elif(Input.is_action_just_released("left")):
+		animated_sprite.play("left")
+		
+	if(Input.is_action_pressed("right")):
+		animated_sprite.play("right_walk")
+	elif(Input.is_action_just_released("right")):
+		animated_sprite.play("right")
